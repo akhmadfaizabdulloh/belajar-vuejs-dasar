abf4a308eb01e16ce08d6c519fe727d8b15f960f
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-05-09 11:26:05 +0700

    raw html

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,9 +1,15 @@
 <script setup> // setup wajib
 console.info("Hello Vue")
+
+const hello = "<b>Hello Vue</b>"
+const helloBro = "<h1>Hello Bro</h1>"
 </script>
 
 <template>
     <h1 class="hello">Hello Vue</h1>
+    <h1 class="hello">{{ hello }}</h1>
+
+    <div v-html="helloBro"></div>
 </template>
 
 <style scoped>
