5bcfb3947add0846a1150120277446749fe56391
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-08-01 13:12:58 +0700

    finishing Template Attributes

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -3,13 +3,45 @@ console.info("Hello Vue")
 
 const hello = "<b>Hello Vue</b>"
 const helloBro = "<h1>Hello Bro</h1>"
+const classHello = "hello"
+
+const id = "hello";
+
+const buttonDisabled = true;
+
+const data = {
+    class: "hello",
+    id:"hello"
+};
+
 </script>
 
 <template>
     <h1 class="hello">Hello Vue</h1>
     <h1 class="hello">{{ hello }}</h1>
 
+    <!-- Kita tidak bisa menggunakan Mustache seperti ini  -->
+    <!-- <h1 class="{{classHello}}">{{ hello }}</h1> -->
+
+    <h1 v-bind:class="classHello">{{ hello }}</h1>
+
     <div v-html="helloBro"></div>
+
+    <div :class="classHello" v-html="helloBro"></div>
+
+    <h1 :id="id" v-bind:class="classHello">{{ hello }}</h1>
+    <h1 :id v-bind:class="classHello">{{ hello }}</h1>
+
+    <!-- Boolean Atrribute -->
+    <button :disabled="buttonDisabled">This is Button</button>
+
+    <!-- Multiple Attribute -->
+    <h1 class="hello">{{ hello }}</h1>
+
+    <!-- Nested Object -->
+    <div :class="data.class" v-html="helloBro"></div>
+
+
 </template>
 
 <style scoped>
