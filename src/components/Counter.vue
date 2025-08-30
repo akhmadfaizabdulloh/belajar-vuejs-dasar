34630a78beeddc705775a72404ba7c74b3449169
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-08-30 11:28:18 +0700

    State & Reactive State

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { ref } from 'vue';
+
 // kita gunakan let karena datanya bisa berubah
-let count = 0;
+// let count = 0;
 
+// reactive state
+console.info(`Load component`);
+let count = ref(0);
+
+// function increment(){
+//     console.info(`Increment counter : ${count}`);
+//     count++;
+
+//     document.getElementById('count').innerText = `Counter : ${count}`;
+// }
+
+// reactive state
 function increment(){
-    console.info(`Increment counter : ${count}`);
-    count++;
-
-    document.getElementById('count').innerText = `Counter : ${count}`;
+    console.info(`Increment counter : ${count.value}`);
+    count.value++;
 }
 
 </script>
