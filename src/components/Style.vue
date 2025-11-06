699d6cd6fc978682d4fedc410a3306dc7b72ac1b
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-11-06 15:17:09 +0700

    chapter: Style - Binding HTML class

---
+++ src/components/Style.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const array = ["red", "bold"];
+
+const red = ref(false);
+const bold = ref(false);
+
+function toggleRed() {
+    red.value = !red.value;  
+}
+
+function toggleBold() {
+    bold.value = !bold.value;
+}
+
+</script>
+
+<template>
+    <h1 :class="{red:red, bold:bold}">Style</h1>
+    <!-- <h1 :class="['red','bold']">Style</h1> -->
+    <h1 :class="array">Style</h1>
+    <button v-on:click="toggleRed">Red</button>
+    <button v-on:click="toggleBold">Bold</button>
+</template>
+
+<style scoped>
+.red {
+    color:red;
+}
+
+.bold {
+    text-transform: uppercase;
+    font-weight: bold;
+}
+</style>
