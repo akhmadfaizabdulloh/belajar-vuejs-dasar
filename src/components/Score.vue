efa70f6a2e57a0911d28c7ecfeb63ca897ede90e
fafaiez <akhmadfaizabdulloh@gmail.com>
2025-11-11 16:11:31 +0700

    chapter: Conditional Rendering

---
+++ src/components/Score.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const score = ref(75);
+</script>
+
+<template>
+    <h1>Score App</h1>
+
+    <div v-show="score > 50">
+        Score is {{ score }}
+    </div>
+
+    <div v-if="score > 90">
+        Yay cool score {{ score }}
+    </div>
+    <div v-else-if="score > 70">
+        Good score {{ score }}
+    </div>
+    <div v-else>
+        Ups, bad score {{ score }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
